b1422b316e3f4669136d05361483377e9aecbf21
Ksalty23 <161716472+Ksalty23@users.noreply.github.com>
2025-01-30 10:55:26 +0000

    Add files via upload

--- COMPSCI PROJ/Graphs.py
+++ COMPSCI PROJ/Graphs.py
@@ -43,20 +43,36 @@ combined_df = pd.concat([freestyle100Men_df, freestyle100Women_df])
 # Create a line plot to compare Men's and Women's Freestyle Results over the years
 fig = px.scatter(combined_df, x='Year', y='Results', color='Gender', title="Men's vs Women's 100m Freestyle Results Over Time", labels={'Results': 'Result', 'Year': 'Year'}, hover_data=['Gender'])
 
-"""allStrokesMen_df = pd.concat([freestyle_df, backstroke_df, breaststroke_df, butterfly_df])
-allStrokesMen_df.drop(columns = ['Unnamed: 0'], inplace = True)
-allStrokesMen_df.drop(columns = ['Unnamed: 0.1'], inplace = True)
-allStrokesMen_df.drop(columns = ['Location'], inplace = True)
 
-womenRemove = allStrokesMen_df[ allStrokesMen_df['Gender'] == 'Women' ].index
-allStrokesMen_df.drop(womenRemove, inplace = True)
+freestyle50Men_df = graphs_df[graphs_df['Stroke'] == 'Freestyle']
 
-print(allStrokesMen_df)
+freestyle50Men_df.drop(columns = ['Unnamed: 0'], inplace = True)
+freestyle50Men_df.drop(columns = ['Unnamed: 0.1'], inplace = True)
+freestyle50Men_df.drop(columns = ['Location'], inplace = True)
+women50Remove = freestyle50Men_df[freestyle50Men_df['Gender'] == 'Women' ].index
+freestyle50Men_df.drop(women50Remove, inplace = True)
+free100DropMen = freestyle50Men_df[freestyle50Men_df['Distance (in meters)'] == '100m'].index
+freestyle50Men_df.drop(free100DropMen, inplace = True)
 
+freestyle50Men_df.to_csv('Freestyle_Men_50.csv')
 
-#This code will create a scatter plot of the 'Year' and 'Results' columns in my dataframe
-fig = px.scatter(allStrokesMen_df, x='Year', y='Results, Distance (in meters), Team', title="Men's Olympic Swimming Results Over Time", labels={'Results': 'Result', 'Distance (in meters)': 'Distance'}, hover_data=['Distance (in meters)', 'Stroke'])"""
+freestyle50Women_df = graphs_df[graphs_df['Stroke'] == 'Freestyle']
 
-"""#This code will display the scatter plot in a new window
-fig.write_html('Olympic_Swimming_Results.html')"""
-fig.write_html('Mens_v_Womens_100m_Freestyle_Results.html')
+freestyle50Women_df.drop(columns = ['Unnamed: 0'], inplace = True)
+freestyle50Women_df.drop(columns = ['Unnamed: 0.1'], inplace = True)
+freestyle50Women_df.drop(columns = ['Location'], inplace = True)
+men50Remove = freestyle50Women_df[freestyle50Women_df['Gender'] == 'Men' ].index
+freestyle50Women_df.drop(men50Remove, inplace = True)
+free100DropWomen = freestyle50Women_df[freestyle50Women_df['Distance (in meters)'] == '100m'].index
+freestyle50Women_df.drop(free100DropWomen, inplace = True)
+
+freestyle50Women_df.to_csv('Freestyle_Women_50.csv')
+
+# Combine the two dataframes for plotting
+freestyle50Men_df['Gender'] = 'Men'
+freestyle50Women_df['Gender'] = 'Women'
+combined50_df = pd.concat([freestyle50Men_df, freestyle50Women_df])
+
+# Create a line plot to compare Men's and Women's Freestyle Results over the years
+fig = px.scatter(combined50_df, x='Year', y='Results', color='Gender', title="Men's vs Women's 50m Freestyle Results Over Time", labels={'Results': 'Result', 'Year': 'Year'}, hover_data=['Gender'])
+fig.write_html('Men_v_Women_Freestyle_50m.html')
